581be242ab95441482dc1ba7d73d5cbb9a8aaadf
Piriquitossauro <alanzani.kaio@gmail.com>
2026-03-15 18:51:52 -0300

    Update BossBarManager.java

--- plugin/src/main/java/me/bardrinks/managers/BossBarManager.java
+++ plugin/src/main/java/me/bardrinks/managers/BossBarManager.java
@@ -14,12 +14,23 @@ public class BossBarManager {
 
     public void updateBar(Player p, int birita) {
 
+        if (birita <= 0) {
+
+            BossBar bar = bars.remove(p.getUniqueId());
+
+            if (bar != null) {
+                bar.removeAll();
+            }
+
+            return;
+        }
+
         BossBar bar = bars.computeIfAbsent(
                 p.getUniqueId(),
                 id -> Bukkit.createBossBar("§6Biritômetro", BarColor.YELLOW, BarStyle.SOLID)
         );
 
-        bar.setProgress(birita / 100.0);
+        bar.setProgress(Math.min(1.0, birita / 100.0));
 
         if (!bar.getPlayers().contains(p))
             bar.addPlayer(p);
