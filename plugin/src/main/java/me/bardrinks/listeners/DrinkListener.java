9e4e75b016089b25d01dde4bd97612b0dfd341e0
Piriquitossauro <alanzani.kaio@gmail.com>
2026-03-15 18:12:09 -0300

    Create DrinkListener.java

---
+++ plugin/src/main/java/me/bardrinks/listeners/DrinkListener.java
@@ -0,0 +1,44 @@
+package me.bardrinks.listeners;
+
+import me.bardrinks.BarDrinks;
+import me.bardrinks.models.DrinkType;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerItemConsumeEvent;
+import org.bukkit.inventory.meta.PotionMeta;
+
+public class DrinkListener implements Listener {
+
+    @EventHandler
+    public void onDrink(PlayerItemConsumeEvent e) {
+
+        if (!(e.getItem().getItemMeta() instanceof PotionMeta meta))
+            return;
+
+        if (!meta.hasCustomModelData())
+            return;
+
+        int model = meta.getCustomModelData();
+
+        Player p = e.getPlayer();
+
+        for (DrinkType type : DrinkType.values()) {
+
+            if (type.getModelData() == model) {
+
+                BarDrinks.get().getDrunkManager().addBirita(p, type.getBirita());
+
+                int birita = BarDrinks.get().getDrunkManager().getBirita(p);
+
+                BarDrinks.get().getBossBarManager().updateBar(p, birita);
+
+                BarDrinks.get().getRankingManager().addDrink(p);
+
+                p.sendMessage("§eVocê bebeu... nível de birita: " + birita);
+
+                return;
+            }
+        }
+    }
+}
