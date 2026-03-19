223acfcd51be7f5f51c9556dca84ebae03d4bf9d
Piriquitossauro <alanzani.kaio@gmail.com>
2026-03-18 22:23:48 -0300

    Update RankingManager.java

--- plugin/src/main/java/me/bardrinks/managers/RankingManager.java
+++ plugin/src/main/java/me/bardrinks/managers/RankingManager.java
@@ -2,19 +2,25 @@ package me.bardrinks.managers;
 
 import org.bukkit.entity.Player;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
+import java.util.*;
 
 public class RankingManager {
 
-    private final Map<UUID, Integer> drinks = new HashMap<>();
+    private final HashMap<UUID, Integer> drinks = new HashMap<>();
 
     public void addDrink(Player p) {
 
-        drinks.put(
-                p.getUniqueId(),
+        drinks.put(p.getUniqueId(),
                 drinks.getOrDefault(p.getUniqueId(), 0) + 1
         );
+    }
+
+    public List<Map.Entry<UUID, Integer>> getTop() {
+
+        List<Map.Entry<UUID, Integer>> list = new ArrayList<>(drinks.entrySet());
+
+        list.sort((a, b) -> b.getValue() - a.getValue());
+
+        return list;
     }
 }
