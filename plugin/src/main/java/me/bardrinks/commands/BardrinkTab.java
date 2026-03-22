3d498eda468aea0ba430440faa64ff3fef59b065
Piriquitossauro <alanzani.kaio@gmail.com>
2026-03-22 18:25:58 -0300

    Update BardrinkTab.java

--- plugin/src/main/java/me/bardrinks/commands/BardrinkTab.java
+++ plugin/src/main/java/me/bardrinks/commands/BardrinkTab.java
@@ -11,6 +11,11 @@ public class BardrinkTab implements TabCompleter {
 
     @Override
     public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
+        
+        // Se o player NÃO tiver permissão de admin, retorna uma lista vazia (não sugere nada)
+        if (!sender.hasPermission("bardrinks.admin")) {
+            return java.util.Collections.emptyList();
+        }
 
         if (args.length == 2) {
             return Arrays.asList("cerveja", "verde", "laranja", "vermelho");
